0de2ee55cfcc58f6f7e88540727ac7891689fbcd
sanguine-hexcraft <hexcraft-apothecary@gmail.com>
2025-11-24 07:12:50 -0700

    added the verbose CL argument

--- main.py
+++ main.py
@@ -2,43 +2,61 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 def main():
     load_dotenv()
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+    
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "One sentence to explain black metal"')
+        print('(rage quitting)')
+        sys.exit(1)
     
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
 
-    args = sys.argv[1:]
+    ask_palantir = " ".join(args)
 
-    if len(sys.argv) == 1:
-        print("We need to use our words, rage quitting!")
-        sys.exit(1)
-    else:
-        ask_palantir = " ".join(args)
+    if verbose:
+        print(f"User prompt: {ask_palantir}\n")
+         
+    # Create a new list of AI queries
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=ask_palantir)]),
+    ]
 
+    generate_content(client, messages, ask_palantir)
+  
+# Get a response from Gemini API with our queries list (messages)
+def generate_content(client, messages, verbose):   
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents=ask_palantir
+        contents=messages,
     )
-
-    print("================================")
-
     
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    # --------- PRINT RESPONSE GOES HERE -----------
+    #
+    if verbose:
+        
+        print("================================")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+        print("================================")
+    
+    print("================================")
     print(f"Palantir Response:\n\n{response.text}")
-
-    
-    print("================================")
-
-    
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-    
-    print("================================")
-
 
 if __name__ == "__main__":
     main()
