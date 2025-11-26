752f0cd58725cff1aa9c406fa12e4bb27188ecba
Sanguine-Hexcraft <hexcraft.apothecary@gmail.com>
2025-11-26 06:49:05 -0700

    Refactor on functions/get_files_info.py and tests.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,10 +4,34 @@ def get_files_info(working_directory, directory="."):
 
     full_path = os.path.join(working_directory, directory)
 
-    # if directory not working_directory:
-        # return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.abspath(full_path).startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    # if directory not directory:
-        # return f'Error: "{directory}" is not a directory'
+    if not os.path.isdir(full_path):
+        return f'Error: "{directory}" is not a directory'
 
-    print(f"TESTICALS: {full_path}")
+    
+    try:
+        lines = []
+        
+        for i in os.listdir(full_path):
+        
+            item_path = os.path.join(full_path, i)
+
+            size = os.path.getsize(item_path)
+
+            is_dir =  os.path.isfile(item_path)
+
+            lines.append(f"- {i}: file_size: {size} bytes, is_dir={is_dir}")
+    
+
+    
+        if directory == ".":
+            intro_line = "Result for current directory:"
+        else:
+            intro_line = f"Result for {directory} directory:"
+
+        return intro_line + "\n" + "\n".join(lines)
+
+    except Exception as e:
+        return f"Error: {e}"
