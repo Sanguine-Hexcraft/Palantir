64ae4fec3f6449fefc242e2249d90b237effc3ea
Sanguine-Hexcraft <hexcraft.apothecary@gmail.com>
2025-11-27 06:04:48 -0700

    added get_file_content, lorem.txt, and new tests.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -8,15 +8,20 @@ def get_file_content(working_directory, file_path):
     full_path = os.path.abspath(full_path)
 
     if not full_path.startswith(abs_path):
-         return f'Error: Cannot read "{full_path})" as it is outside the permitted working directory'
+         return f'Error: Cannot read "{full_path}" as it is outside the permitted working directory'
 
     if not os.path.isfile(full_path):
         return f'Error: File not found or is not a regular file: "{file_path}"' 
 
     try:
         with open(full_path, "r") as f:
-            file_content_string = f.read(MAX_CHARACTERS) + f'[...File "{file_path}" truncated at 10000 characters]'
-            print(file_content_string)
+            file_content_string = f.read(MAX_CHARACTERS)
+
+            if os.path.getsize(full_path) > MAX_CHARACTERS:
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARACTERS} characters]' 
+
+            return file_content_string   
+            
     except Exception as e:
         return f"Error: {e}"
         
