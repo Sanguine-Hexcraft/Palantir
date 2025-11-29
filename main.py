021d475726dc235a9a4042dcf008b7aee4b8fe86
sanguine-hexcraft <hexcraft-apothecary@gmail.com>
2025-11-28 20:29:13 -0700

    added prompt.py and silly little forced system prompt

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+from re import S
 import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 
 def main():
@@ -37,10 +39,13 @@ def main():
     generate_content(client, messages, verbose)
   
 # Get a response from Gemini API with our queries list (messages)
-def generate_content(client, messages, verbose):   
+def generate_content(client, messages, verbose):
+
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     
     prompt_tokens = response.usage_metadata.prompt_token_count
